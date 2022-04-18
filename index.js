046c196b189c046bc809389b0720e83e80f8b64a
Azibatayam <95300506+Azibatayam@users.noreply.github.com>
2022-04-18 10:35:43 +0200

    Data types

---
+++ index.js
@@ -0,0 +1,12 @@
+/* Exercise 1*/
+
+Number - This is any number between -(2.53 -1) and +(2.53-1) it can be an integer or  floating number.
+String - These are different types of characters put in a qoute. 
+Boolean - The values of a boolean is either true or false and it used in conditionals.If a conditiion given is truthy or falsy. 
+Undefined - This is a container or variable given without a value assigned.
+Null - This container or variable given with the value null. This can be is done this way so in the future another value can be assigned to that variable.
+
+/* Exercise2 */
+
+An object is a collection of key value peers, it has a key followed by a colon and then a value. 
+
